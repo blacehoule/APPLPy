615348cb4eea6927706c817504a8e29e1ce63440
Matt Robinson <mthw.wm.robinson@gmail.com>
2026-03-27 15:24:16 -0400

    feat: add Rust-backed discrete convolution for random variables (#40)
    * first pass on convolution
    
    * corrections for conv
    
    * add tests for convolution
    
    * add and addassign for rv
    
    * add convolution into the python api
    
    * patch new discrete functions into python

--- rust/src/algorithms/algebra.rs
+++ rust/src/algorithms/algebra.rs
@@ -129,6 +129,131 @@ pub fn product_discrete(
     Ok(product_rv)
 }
 
+/// Computes the sum of two independent discrete random variables
+///
+/// # Arguments
+/// * `random_variable_1` - the first random variable
+/// * `random_variable_2` - the second random variable
+///
+/// # Returns
+/// * `sum_rv` - the product of the two random variables
+///
+/// # Examples
+/// ```
+/// use applpy_rust::algorithms::algebra::convolution_discrete;
+/// use applpy_rust::algorithms::number::Number;
+/// use applpy_rust::algorithms::rv::{DomainType, FunctionalForm, RandomVariable};
+/// use num_rational::Rational64;
+///
+/// let rv1 = RandomVariable {
+///     function: vec![
+///         Number::Rational(Rational64::new(1, 2)),
+///         Number::Rational(Rational64::new(1, 2)),
+///     ],
+///     support: vec![Number::Integer(1), Number::Integer(2)],
+///     functional_form: FunctionalForm::Pdf,
+///     domain_type: DomainType::Discrete,
+/// };
+///
+/// let rv2 = RandomVariable {
+///     function: vec![
+///         Number::Rational(Rational64::new(1, 2)),
+///         Number::Rational(Rational64::new(1, 2)),
+///     ],
+///     support: vec![Number::Integer(2), Number::Integer(3)],
+///     functional_form: FunctionalForm::Pdf,
+///     domain_type: DomainType::Discrete,
+/// };
+///
+/// let sum = convolution_discrete(&rv1, &rv2).unwrap();
+///
+/// assert_eq!(
+///     sum.support,
+///     vec![Number::Integer(3), Number::Integer(4), Number::Integer(5)]
+/// );
+/// assert_eq!(
+///     sum.function,
+///     vec![
+///         Number::Rational(Rational64::new(1, 4)),
+///         Number::Rational(Rational64::new(1, 2)),
+///         Number::Rational(Rational64::new(1, 4)),
+///     ]
+/// );
+/// assert!(sum.verify_pdf(None).unwrap());
+/// ```
+pub fn convolution_discrete(
+    random_variable_1: &RandomVariable,
+    random_variable_2: &RandomVariable,
+) -> Result<RandomVariable, String> {
+    let pdf_random_variable_1 = random_variable_1.to_pdf()?;
+    let function_1 = pdf_random_variable_1.function;
+    let support_1 = pdf_random_variable_1.support;
+
+    let pdf_random_variable_2 = random_variable_2.to_pdf()?;
+    let function_2 = pdf_random_variable_2.function;
+    let support_2 = pdf_random_variable_2.support;
+
+    // Find the values and probabilities for support_1 + support_2
+    // for all combinations of support values
+    let mut raw_conv_support = Vec::new();
+    for &s1 in support_1.iter() {
+        for &s2 in support_2.iter() {
+            let support_sum = s1 + s2;
+            raw_conv_support.push(support_sum);
+        }
+    }
+
+    let mut raw_conv_function = Vec::new();
+    for &f1 in function_1.iter() {
+        for &f2 in function_2.iter() {
+            let probability = f1 * f2;
+            raw_conv_function.push(probability);
+        }
+    }
+
+    // Sorts the results by the support values
+    let mut raw_conv_pairs: Vec<_> = raw_conv_support
+        .into_iter()
+        .zip(raw_conv_function)
+        .collect();
+
+    raw_conv_pairs.sort_by(|a, b| {
+        let first_value = a.0.to_f64();
+        let second_value = b.0.to_f64();
+        first_value.total_cmp(&second_value)
+    });
+
+    let (sorted_support, sorted_function): (Vec<Number>, Vec<Number>) =
+        raw_conv_pairs.into_iter().unzip();
+
+    // Remove redundant elements from the support
+    let mut conv_support = Vec::new();
+    let mut conv_function = Vec::new();
+
+    for (&s, &f) in sorted_support.iter().zip(sorted_function.iter()) {
+        let support_index = conv_support.iter().position(|&x| x == s);
+
+        match support_index {
+            Some(index) => {
+                conv_function[index] += f;
+            }
+            None => {
+                conv_support.push(s);
+                conv_function.push(f);
+            }
+        }
+    }
+
+    let sum_rv = RandomVariable {
+        function: conv_function,
+        support: conv_support,
+        functional_form: FunctionalForm::Pdf,
+        domain_type: DomainType::Discrete,
+    };
+
+    Ok(sum_rv)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -234,5 +359,91 @@ mod tests {
         assert_eq!(product.functional_form, FunctionalForm::Pdf);
         assert_eq!(product.domain_type, DomainType::Discrete);
         assert!(product.verify_pdf(None).unwrap());
+    }
+
+    #[test]
+    fn convolution_discrete_combines_duplicate_support_values() {
+        let rv1 = two_point_pdf(
+            [0, 1],
+            [Rational64::new(1, 2), Rational64::new(1, 2)],
+            FunctionalForm::Pdf,
+        );
+        let rv2 = two_point_pdf(
+            [2, 3],
+            [Rational64::new(1, 5), Rational64::new(4, 5)],
+            FunctionalForm::Pdf,
+        );
+
+        let sum = convolution_discrete(&rv1, &rv2).unwrap();
+
+        assert_eq!(
+            sum.support,
+            vec![Number::Integer(2), Number::Integer(3), Number::Integer(4)]
+        );
+        assert_eq!(
+            sum.function,
+            vec![
+                Number::Rational(Rational64::new(1, 10)),
+                Number::Rational(Rational64::new(1, 2)),
+                Number::Rational(Rational64::new(2, 5))
+            ]
+        );
+        assert!(sum.verify_pdf(None).unwrap());
+    }
+
+    #[test]
+    fn convolution_discrete_accepts_cdf_inputs_by_converting_to_pdf() {
+        let rv1 = two_point_pdf(
+            [1, 2],
+            [Rational64::new(1, 4), Rational64::new(1, 1)],
+            FunctionalForm::Cdf,
+        );
+        let rv2 = two_point_pdf(
+            [3, 5],
+            [Rational64::new(1, 2), Rational64::new(1, 1)],
+            FunctionalForm::Cdf,
+        );
+
+        let sum = convolution_discrete(&rv1, &rv2).unwrap();
+
+        assert_eq!(
+            sum.support,
+            vec![
+                Number::Integer(4),
+                Number::Integer(5),
+                Number::Integer(6),
+                Number::Integer(7)
+            ]
+        );
+        assert_eq!(
+            sum.function,
+            vec![
+                Number::Rational(Rational64::new(1, 8)),
+                Number::Rational(Rational64::new(3, 8)),
+                Number::Rational(Rational64::new(1, 8)),
+                Number::Rational(Rational64::new(3, 8))
+            ]
+        );
+        assert!(sum.verify_pdf(None).unwrap());
+    }
+
+    #[test]
+    fn convolution_discrete_preserves_pdf_and_discrete_domain() {
+        let rv1 = two_point_pdf(
+            [2, 4],
+            [Rational64::new(1, 3), Rational64::new(2, 3)],
+            FunctionalForm::Pdf,
+        );
+        let rv2 = two_point_pdf(
+            [1, 3],
+            [Rational64::new(3, 10), Rational64::new(7, 10)],
+            FunctionalForm::Pdf,
+        );
+
+        let sum = convolution_discrete(&rv1, &rv2).unwrap();
+
+        assert_eq!(sum.functional_form, FunctionalForm::Pdf);
+        assert_eq!(sum.domain_type, DomainType::Discrete);
+        assert!(sum.verify_pdf(None).unwrap());
     }
 }
